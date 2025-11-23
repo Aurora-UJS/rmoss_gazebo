2813e017baf0d56378bc4bcb09c89b8bbdcded7c
xlqmu <3212929002@qq.com>
2025-11-23 15:16:31 +0800

    delete compilation artifacts

--- build/rmoss_gz_base/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/xlqmu/rmoss_gazebo/install/rmoss_gz_plugins/share/rmoss_gz_plugins/package.sh"
--- build/rmoss_gz_plugins/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xlqmu/rmoss_gazebo/rmoss_gz_plugins
-# Build directory: /home/xlqmu/rmoss_gazebo/build/rmoss_gz_plugins
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
